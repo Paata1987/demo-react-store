62f58e2726e426cb616a7e836e813e4d8e15e790
Paata Home <paata.tepnadze@gmail.com>
2023-04-29 23:32:02 +0200

    add service fetching data and getting category

--- src/service/api.js
+++ src/service/api.js
@@ -3,8 +3,21 @@ import axios from "axios";
 export const fetchProducts = async () => {
     try {
         const response = await axios.get("https://fakestoreapi.com/products");
-        return response.data;
+        const products = response.data;
+        const categories = removeDuplicatesNames(products.map(product => product.category));
+        return { response, categories }
     } catch (error) {
         console.error(error);
     }
 };
+
+
+function removeDuplicatesNames(categoryArray) {
+    const category = [];
+    categoryArray.forEach(element => {
+        if (!category.includes(element)) {
+            category.push(element)
+        }
+    });
+    return category
+}
