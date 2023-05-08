918f139c1809e204bcb0d9e4c63627a1d5b1a529
Paata Tepnadze <74315733+Paata1987@users.noreply.github.com>
2023-05-08 10:47:26 +0200

    create modal windows/ show product img

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Button, Modal } from "react-bootstrap";
+import { Button, Modal, Row, Col, Image } from "react-bootstrap";
 
 const ProductModal = (props) => {
-  const { title, description, price, rate, showModal, onHide } = props;
+  const { title, description, price, rate, showModal, onHide, img } = props;
 
   const handleClose = () => {
     onHide();
@@ -14,19 +14,36 @@ const ProductModal = (props) => {
         <Modal.Header closeButton>
           <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
-
-        <Modal.Body>
-          <div>description: {description}</div>
-          <p>
-            price: {price} rate: {rate}
-          </p>
-        </Modal.Body>
+        <Row>
+          <Col>
+            <Image xs={6} md={4} src={img} className="col-4" rounded />
+          </Col>
+          <Col>
+            <Modal.Body>
+              <div>description: {description}</div>
+              <p>
+                price: {price} rate: {rate}
+              </p>
+            </Modal.Body>
+          </Col>
+        </Row>
 
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Save changes</Button>
+          <Button variant="primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-cart4 m-2"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+            </svg>
+          </Button>
         </Modal.Footer>
       </Modal>
     </>
